321cde8c56da118126b2d24aa783dc2efa8bde36
bochkarev.semen <aiscl@ya.ru>
2025-02-05 20:34:13 +0300

    try oauth

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 import io
 import os
 
+import google_auth_oauthlib.flow
 import googleapiclient.discovery
+import googleapiclient.discovery
+import googleapiclient.errors
 import uvicorn
 from fastapi import FastAPI
 from googleapiclient.http import MediaIoBaseDownload
 from pydantic import BaseModel
 
-from config import YOUTUBE_API_KEY
+from config import SECRET_OAUTH_JSON
 
 app = FastAPI()
+
+client_secrets_file = "CLIENT_SECRET_FILE.json"
+
+with open(client_secrets_file, 'w', encoding='utf-8') as f:
+    f.write(SECRET_OAUTH_JSON)
 
 
 class GetTranscriptBody(BaseModel):
@@ -20,8 +28,12 @@ class GetTranscriptBody(BaseModel):
 def get_subtitles(video_id: str, lang: str = 'ru'):
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        client_secrets_file, ["https://www.googleapis.com/auth/youtube.force-ssl"])
+    credentials = flow.run_console()
+
     youtube = googleapiclient.discovery.build(
-        "youtube", "v3", developerKey=YOUTUBE_API_KEY)
+        "youtube", "v3", credentials=credentials)
 
     request = youtube.captions().download(id=video_id, tlang=lang)
 
--- config.py
+++ config.py
@@ -4,4 +4,5 @@ import dotenv
 
 dotenv.load_dotenv()
 
-YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
+# YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
+SECRET_OAUTH_JSON = os.getenv("SECRET_OAUTH_JSON")
