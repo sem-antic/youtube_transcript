76b079b654a8fe0605d3908289c851b7a2751c9f
bochkarev.semen <aiscl@ya.ru>
2025-02-05 20:14:25 +0300

    try google api

--- app.py
+++ app.py
@@ -1,20 +1,49 @@
+import io
+import os
+
+import googleapiclient.discovery
 import uvicorn
 from fastapi import FastAPI
+from googleapiclient.http import MediaIoBaseDownload
 from pydantic import BaseModel
-from youtube_transcript_api import YouTubeTranscriptApi
+
+from config import YOUTUBE_API_KEY
 
 app = FastAPI()
 
 
 class GetTranscriptBody(BaseModel):
     video_id: str
-    languages: list[str] = ['en']
+    language: str = 'ru'
+
+
+def get_subtitles(video_id: str, lang: str = 'ru'):
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    youtube = googleapiclient.discovery.build(
+        "youtube", "v3", developerKey=YOUTUBE_API_KEY)
+
+    request = youtube.captions().download(id=video_id, tlang=lang)
+
+    fname = f"files/{video_id}"
+    fh = io.FileIO(fname, "wb")
+
+    download = MediaIoBaseDownload(fh, request)
+    complete = False
+    while not complete:
+        status, complete = download.next_chunk()
+
+    with open(fname, 'r', encoding='utf-8') as f:
+        data = f.read()
+
+    os.remove(fname)
+    return data
 
 
 @app.post("/api/v1/get_transcript")
 async def predict_customer_intents(body: GetTranscriptBody):
-    result = YouTubeTranscriptApi.get_transcript(body.video_id, languages=body.languages)
-    return result
+    data = get_subtitles(body.video_id)
+    return data
 
 
 if __name__ == "__main__":
---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+import dotenv
+
+dotenv.load_dotenv()
+
+YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
